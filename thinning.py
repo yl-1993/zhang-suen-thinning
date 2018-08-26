ae8e9733e9e0f20df477b393e4b5757491716eec
yl-1993 <yl93528@gmail.com>
2018-08-26 12:24:44 +0800

    use numba.jit to replace weave deps

--- thinning.py
+++ thinning.py
@@ -1,39 +1,33 @@
-from scipy import weave
 import numpy as np
 import cv2
 import sys
+from numba import jit
 
-def _thinningIteration(im, iter):
-	I, M = im, np.zeros(im.shape, np.uint8)
-	expr = """
-	for (int i = 1; i < NI[0]-1; i++) {
-		for (int j = 1; j < NI[1]-1; j++) {
-			int p2 = I2(i-1, j);
-			int p3 = I2(i-1, j+1);
-			int p4 = I2(i, j+1);
-			int p5 = I2(i+1, j+1);
-			int p6 = I2(i+1, j);
-			int p7 = I2(i+1, j-1);
-			int p8 = I2(i, j-1);
-			int p9 = I2(i-1, j-1);
+@jit
+def _thinningIteration(im, iter_):
+    M = np.zeros(im.shape, np.uint8)
+    h, w = im.shape
+    for i in range(1, h - 1):
+        for j in range(1, w - 1):
+            p2 = im[i - 1, j]
+            p3 = im[i - 1, j + 1]
+            p4 = im[i, j + 1]
+            p5 = im[i + 1, j + 1]
+            p6 = im[i + 1, j]
+            p7 = im[i + 1, j - 1]
+            p8 = im[i, j - 1]
+            p9 = im[i - 1, j - 1]
+            A = (p2 == 0 and p3 == 1) + (p3 == 0 and p4 == 1) + \
+                (p4 == 0 and p5 == 1) + (p5 == 0 and p6 == 1) + \
+                (p6 == 0 and p7 == 1) + (p7 == 0 and p8 == 1) + \
+                (p8 == 0 and p9 == 1) + (p9 == 0 and p2 == 1)
+            B = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9
+            m1 = (p2 * p4 * p6) if (iter_ == 0) else (p2 * p4 * p8)
+            m2 = (p4 * p6 * p8) if (iter_ == 0) else (p2 * p6 * p8)
+            if A == 1 and B >= 2 and B <=6 and m1 == 0 and m2 == 0:
+                M[i, j] = 1
 
-			int A  = (p2 == 0 && p3 == 1) + (p3 == 0 && p4 == 1) +
-			         (p4 == 0 && p5 == 1) + (p5 == 0 && p6 == 1) +
-			         (p6 == 0 && p7 == 1) + (p7 == 0 && p8 == 1) +
-			         (p8 == 0 && p9 == 1) + (p9 == 0 && p2 == 1);
-			int B  = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
-			int m1 = iter == 0 ? (p2 * p4 * p6) : (p2 * p4 * p8);
-			int m2 = iter == 0 ? (p4 * p6 * p8) : (p2 * p6 * p8);
-
-			if (A == 1 && B >= 2 && B <= 6 && m1 == 0 && m2 == 0) {
-				M2(i,j) = 1;
-			}
-		}
-	} 
-	"""
-
-	weave.inline(expr, ["I", "iter", "M"])
-	return (I & ~M)
+    return im & ~M
 
 
 def thinning(src):
@@ -51,6 +45,7 @@ def thinning(src):
 
 	return dst * 255
 
+
 if __name__ == "__main__":
 	src = cv2.imread("kanji.png")
 	if src == None:
@@ -61,4 +56,3 @@ if __name__ == "__main__":
 	cv2.imshow("src", bw)
 	cv2.imshow("thinning", bw2)
 	cv2.waitKey()
-
